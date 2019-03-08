ab80c6f8623ec001d71b9a19d20b491bcca081bd
lishibang <lsb3524@163.com>
2019-03-08 15:40:24 +0800

    add spring security oauth2 service

---
+++ springcloud.authentication.service/src/main/java/com/springcloud/authentication/service/Application.java
@@ -0,0 +1,17 @@
+package com.springcloud.authentication.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+@EnableAuthorizationServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
